b1dc3a636e5bb4419d067c8079967f3f80fd6637
Sena Wiguna <senaashari06@gmail.com>
2025-05-14 08:57:50 +0700

    upload project

---
+++ App.js
@@ -0,0 +1,25 @@
+
+// import { Text } from "react-native";
+
+// function App() {
+//     return (
+//         <Text>sena</Text>
+//     )
+// }
+
+// export default App;
+// color
+// fontSize
+// fontWeight
+
+import { Text , View } from "react-native";
+
+function App() {
+    return (
+        <View>
+            <Text style={{color : "pink", fontSize: 30, fontStyle: "italic", fontWeight: "bold", fontFamily: "serif"}}>Sena Laguna</Text>
+        </View>
+    )
+}
+
+export default App;
